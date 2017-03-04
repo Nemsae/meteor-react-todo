ee00027bfcff1017cde4da3fb30db88c636aa548
John Son <jsson77@gmail.com>
2017-03-04 11:41:17 +0100

    first render

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class Task extends Component {
+  render() {
+    return (
+      <li>{this.props.task.text}</li>
+    )
+  }
+}
+
+Task.propTypes = {
+  task: PropTypes.object.isRequired,
+}
+
+console.log('PropTypes from React: ', PropTypes);
+console.log('Task.propTypes: ', Task.propTypes);
